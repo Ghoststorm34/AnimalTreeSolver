9fc1e050369eba9e48eb49554ccc4ec4dc22a84b
Alexander Ayers <alexayers2000@gmail.com>
2021-04-21 18:45:46 -0400

    Added basic template for project implementation

---
+++ CS3151AnimalTreeAlexanderAyers/src/model/Response.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Response {
+	private String value;
+	private ResponseType type;
+	
+	public Response(String value, ResponseType type) {
+		this.value = value;
+		this.type = type;
+	}
+}
