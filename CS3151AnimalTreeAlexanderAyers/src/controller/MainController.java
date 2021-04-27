d8d5fdc6a72481ea8b6a91f1d1551c8534ddd557
Alexander Ayers <alexayers2000@gmail.com>
2021-04-27 18:38:24 -0400

    Added basic controller skeleton.

--- CS3151AnimalTreeAlexanderAyers/src/controller/MainController.java
+++ CS3151AnimalTreeAlexanderAyers/src/controller/MainController.java
@@ -1,5 +1,35 @@
 package controller;
 
-public class MainController {
+import model.AnimalTree;
 
+/**
+ * Controller for whole GUI.
+ * @author Alexander Ayers
+ * @version Spring 2021
+ *
+ */
+public class MainController {
+	private AnimalTree tree;
+	
+	/**
+	 * Zero-parameter constructor.
+	 * 
+	 * @precondition none
+	 * @postcondition getTree() != null
+	 */
+	public MainController() {
+		this.tree = new AnimalTree();
+	}
+	
+	/**
+	 * Gets the tree.
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return the tree
+	 */
+	public AnimalTree getTree() {
+		return this.tree;
+	}
 }
