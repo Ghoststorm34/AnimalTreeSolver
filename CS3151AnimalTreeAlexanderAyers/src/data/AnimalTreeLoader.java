ee50b1a6ef416ff399c8081595e6225b4fd1ce73
Alexander Ayers <alexayers2000@gmail.com>
2021-04-30 12:49:15 -0400

    Added documentation for saver and loader.

--- CS3151AnimalTreeAlexanderAyers/src/data/AnimalTreeLoader.java
+++ CS3151AnimalTreeAlexanderAyers/src/data/AnimalTreeLoader.java
@@ -1,5 +1,11 @@
 package data;
 
+/**
+ * Loads an animal tree from a file.
+ * @author Alexander Ayers
+ * @version Spring 2021
+ *
+ */
 public class AnimalTreeLoader {
 
 }
--- CS3151AnimalTreeAlexanderAyers/src/data/AnimalTreeSaver.java
+++ CS3151AnimalTreeAlexanderAyers/src/data/AnimalTreeSaver.java
@@ -1,5 +1,11 @@
 package data;
 
+/**
+ * Saves an animal tree to a file.
+ * @author Alexander Ayers
+ * @version Spring 2021
+ *
+ */
 public class AnimalTreeSaver {
 
 }
